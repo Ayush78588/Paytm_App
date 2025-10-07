eaeb5f022ba80e1979f11f91a72299eebf0c2138
Ayush Kumar <ayushkumar78588@gmail.com>
2025-10-07 12:25:05 +0530

    fixed error

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import UserBalance from "../components/userBalance"
+import UserBalance from "../components/UserBalance"
 import FilterUsers from "../components/FilterUsers"
 import Header from "../components/Header"
 import User from "../components/User"
